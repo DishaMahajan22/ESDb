4d7ff7394d354eaaafc25ac022b1d38a255b7754
Disha Mahajan <disham2004@gmail.com>
2023-11-10 22:14:37 -0800

    removed uneccessary starter images and text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
           Welcome to ESDB!
         </h1>
         <p>Your database for everything E-Sports!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
